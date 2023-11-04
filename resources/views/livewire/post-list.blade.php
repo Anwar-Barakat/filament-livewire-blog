f996856d65595c50371760bc607cfa685d077849
Anwar-Barakat <brkatanwar0@gmail.com>
2023-11-04 08:55:14 +0300

    using alpinejs to make search on the blog page

--- resources/views/livewire/post-list.blade.php
+++ resources/views/livewire/post-list.blade.php
@@ -1,6 +1,9 @@
 <div class="md:col-span-3 col-span-4">
     <div id="posts" class=" px-3 lg:px-7 py-6">
         <div class="flex justify-between items-center border-b border-gray-100">
+            <div class="text-gray-600">
+                {{ $search ? "Searching ({$search})" : "" }}
+            </div>
             <div id="filter-selector" class="flex items-center space-x-4 font-light ">
                 <button class="text-gray-500 py-4 {{ $this->sort ==='desc' ? 'border-b border-gray-700 text-gray-900' :'' }}" wire:click="setSort('desc')">Latest</button>
                 <button class="text-gray-500 py-4 {{ $this->sort ==='asc' ? 'border-b border-gray-700 text-gray-900' :'' }} " wire:click="setSort('asc')">Oldest</button>
